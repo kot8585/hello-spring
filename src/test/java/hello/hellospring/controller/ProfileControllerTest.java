fee228f0c2109374aeffea22e827ea63d9e0f466
kot8585 <ansgywjd321@gmail.com>
2021-05-31 18:04:13 +0900

    왜 안되지

---
+++ src/test/java/hello/hellospring/controller/ProfileControllerTest.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfileControllerTest {
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
